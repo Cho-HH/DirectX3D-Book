d7d005a7e4dc24a0ca92167dcdadc32ffab03edd
HyunHo <chh1880@gmail.com>
2020-08-11 18:38:49 +0900

    q

---
+++ Chapter6/DirectX_FrameWork/BoxApp.cpp
@@ -0,0 +1,261 @@
+#include "stdafx.h"
+#include "BoxApp.h"
+
+BoxApp::BoxApp(HINSTANCE hInstance) :
+	FrameWork(hInstance),
+	m_Theta(1.5f * MathHelper::Pi),
+	m_Phi(0.25 * MathHelper::Pi),
+	m_Radius(5.0f)
+
+{
+	m_MainWndCaption = L"Box Demo";
+
+	m_LastMousePos.x = 0;
+	m_LastMousePos.y = 0;
+
+	XMMATRIX I = XMMatrixIdentity();
+	XMStoreFloat4x4(&m_World, I);
+	XMStoreFloat4x4(&m_View, I);
+	XMStoreFloat4x4(&m_Proj, I);
+}
+
+bool BoxApp::Init()
+{
+	if (!FrameWork::Init())
+		return false;
+
+	BuildGeometryBuffers();
+	BuildFX();
+	BuildVertexLayout();
+
+	return true;
+}
+
+void BoxApp::Update(float dt)
+{
+	float x = m_Radius * sinf(m_Phi) * cosf(m_Theta);
+	float z = m_Radius * sinf(m_Phi) * sinf(m_Theta);
+	float y = m_Radius * cosf(m_Phi);
+
+	XMVECTOR pos = XMVectorSet(x, y, z, 1.0f);
+	XMVECTOR target = XMVectorZero();
+	XMVECTOR up = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
+
+	XMMATRIX V = XMMatrixLookAtLH(pos, target, up);
+	XMStoreFloat4x4(&m_View, V);
+}
+
+void BoxApp::RenderFrame()
+{
+	float color[4] = { 0.0f, 1.0f, 1.0f, 1.0f };
+	m_spDeviceCon->ClearRenderTargetView(m_spTargetView.Get(), color);
+	m_spDeviceCon->ClearDepthStencilView(m_spDepthStencilView.Get(),
+		D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL,
+		1.0f,
+		0);
+
+	m_spDeviceCon->IASetInputLayout(m_InputLayout.Get());
+	m_spDeviceCon->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+	UINT stride = sizeof(Vertex);
+	UINT offset = 0;
+	m_spDeviceCon->IASetVertexBuffers(0, 1, m_BoxVB.GetAddressOf(), &stride, &offset);
+	m_spDeviceCon->IASetIndexBuffer(m_BoxIB.Get(), DXGI_FORMAT_R32_UINT, 0);
+
+	// Set constants
+	XMMATRIX world = XMLoadFloat4x4(&m_World);
+	XMMATRIX view = XMLoadFloat4x4(&m_View);
+	XMMATRIX proj = XMLoadFloat4x4(&m_Proj);
+	XMMATRIX worldViewProj = world * view * proj;
+
+	m_fxWorldViewProj->SetMatrix(reinterpret_cast<float*>(&worldViewProj));
+
+	D3DX11_TECHNIQUE_DESC techDesc;
+	m_Tech->GetDesc(&techDesc);
+	for (UINT p = 0; p < techDesc.Passes; ++p)
+	{
+		m_Tech->GetPassByIndex(p)->Apply(0, m_spDeviceCon.Get());
+
+		// 36 indices for the box.
+		m_spDeviceCon->DrawIndexed(36, 0, 0);
+	}
+
+	m_spSwapchain->Present(0, 0);
+}
+
+void BoxApp::OnResize()
+{
+	FrameWork::OnResize();
+
+	XMMATRIX P = XMMatrixPerspectiveFovLH(0.25f * MathHelper::Pi, AspectRatio(), 1.0f, 1000.0f);
+	XMStoreFloat4x4(&m_Proj, P);
+}
+
+void BoxApp::OnMouseDown(WPARAM btnState, int x, int y)
+{
+	m_LastMousePos.x = x;
+	m_LastMousePos.y = y;
+
+	SetCapture(m_hWnd);
+}
+
+void BoxApp::OnMouseUp(WPARAM btnState, int x, int y)
+{
+	ReleaseCapture();
+}
+
+void BoxApp::OnMouseMove(WPARAM btnState, int x, int y)
+{
+	if ((btnState & MK_LBUTTON) != 0)
+	{
+		float dx = XMConvertToRadians(0.25f * static_cast<float>(x - m_LastMousePos.x));
+		float dy = XMConvertToRadians(0.25f * static_cast<float>(y - m_LastMousePos.y));
+
+		m_Theta += dx;
+		m_Phi += dy;
+
+		m_Phi = MathHelper::Clamp(m_Phi, 0.1f, MathHelper::Pi - 0.1f);
+	}
+	else if ((btnState & MK_RBUTTON) != 0)
+	{
+		float dx = 0.005f * static_cast<float>(x - m_LastMousePos.x);
+		float dy = 0.005f * static_cast<float>(y - m_LastMousePos.y);
+
+		m_Radius += dx - dy;
+
+		m_Radius = MathHelper::Clamp(m_Radius, 3.0f, 15.0f);
+	}
+
+	m_LastMousePos.x = x;
+	m_LastMousePos.y = y;
+}
+
+void BoxApp::BuildGeometryBuffers()
+{
+	Vertex vertices[] =
+	{
+		{ XMFLOAT3(-1.0f, -1.0f, -1.0f), XMFLOAT4((const float*)&Colors::White  ) },
+		{ XMFLOAT3(-1.0f, +1.0f, -1.0f), XMFLOAT4((const float*)&Colors::Black  ) },
+		{ XMFLOAT3(+1.0f, +1.0f, -1.0f), XMFLOAT4((const float*)&Colors::Red    ) },
+		{ XMFLOAT3(+1.0f, -1.0f, -1.0f), XMFLOAT4((const float*)&Colors::Green  ) },
+		{ XMFLOAT3(-1.0f, -1.0f, +1.0f), XMFLOAT4((const float*)&Colors::Blue   ) },
+		{ XMFLOAT3(-1.0f, +1.0f, +1.0f), XMFLOAT4((const float*)&Colors::Yellow ) },
+		{ XMFLOAT3(+1.0f, +1.0f, +1.0f), XMFLOAT4((const float*)&Colors::Cyan   ) },
+		{ XMFLOAT3(+1.0f, -1.0f, +1.0f), XMFLOAT4((const float*)&Colors::Magenta) }
+	};
+
+	D3D11_BUFFER_DESC vbd;
+	vbd.Usage = D3D11_USAGE_IMMUTABLE;
+	vbd.ByteWidth = sizeof(Vertex) * 8;
+	vbd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	vbd.CPUAccessFlags = 0;
+	vbd.MiscFlags = 0;
+	vbd.StructureByteStride = 0;
+	D3D11_SUBRESOURCE_DATA vinitData;
+	vinitData.pSysMem = vertices;
+	m_spDevice->CreateBuffer(&vbd, &vinitData, &m_BoxVB);
+
+	//왼손좌표계
+	UINT indices[] = {
+		// front face
+		0, 1, 2,
+		0, 2, 3,
+
+		// back face
+		4, 6, 5,
+		4, 7, 6,
+
+		// left face
+		4, 5, 1,
+		4, 1, 0,
+
+		// right face
+		3, 2, 6,
+		3, 6, 7,
+
+		// top face
+		1, 5, 6,
+		1, 6, 2,
+
+		// bottom face
+		4, 0, 3,
+		4, 3, 7
+	};
+
+	D3D11_BUFFER_DESC ibd;
+	ibd.Usage = D3D11_USAGE_IMMUTABLE;
+	ibd.ByteWidth = sizeof(UINT) * 36;
+	ibd.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	ibd.CPUAccessFlags = 0;
+	ibd.MiscFlags = 0;
+	ibd.StructureByteStride = 0;
+	D3D11_SUBRESOURCE_DATA iinitData;
+	iinitData.pSysMem = indices;
+	m_spDevice->CreateBuffer(&ibd, &iinitData, &m_BoxIB);
+}
+
+void BoxApp::BuildFX()
+{
+	DWORD shaderFlags = 0;
+#if defined( DEBUG ) || defined( _DEBUG )
+	shaderFlags |= D3D10_SHADER_DEBUG;
+	shaderFlags |= D3D10_SHADER_SKIP_OPTIMIZATION;
+#endif
+
+	ComPtr<ID3D10Blob> compiledShader;
+	ComPtr<ID3D10Blob> compilationMsgs;
+	//효과파일을 compiledShader에 담아준다.
+	HRESULT hr = D3DCompileFromFile(L"FX/color.fx", 0, 0, 0, "fx_5_0", shaderFlags, 0, compiledShader.GetAddressOf(), compilationMsgs.GetAddressOf());
+
+	// compilationMsgs can store errors or warnings.
+	if (compilationMsgs != 0)
+	{
+		MessageBoxA(0, (char*)compilationMsgs->GetBufferPointer(), 0, 0);
+	}
+
+	if (FAILED(hr))
+	{
+		OutputShaderErrorMsg(compilationMsgs.Get(), m_hWnd, L"FX/color.fx");
+	}
+
+	D3DX11CreateEffectFromMemory(compiledShader->GetBufferPointer(), compiledShader->GetBufferSize(),
+		0, m_spDevice.Get(), m_FX.GetAddressOf());
+
+	m_Tech = m_FX->GetTechniqueByName("ColorTech");
+	m_fxWorldViewProj = m_FX->GetVariableByName("gWorldViewProj")->AsMatrix();
+}
+
+void BoxApp::BuildVertexLayout()
+{
+	D3D11_INPUT_ELEMENT_DESC vertexDesc[] =
+	{
+		{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0},
+		{"COLOR",    0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0}
+	};
+
+	D3DX11_PASS_DESC passDesc;
+	m_Tech->GetPassByIndex(0)->GetDesc(&passDesc);
+	m_spDevice->CreateInputLayout(vertexDesc, 2, passDesc.pIAInputSignature,
+		passDesc.IAInputSignatureSize, &m_InputLayout);
+}
+
+void BoxApp::OutputShaderErrorMsg(ID3D10Blob* errMsg, HWND hWnd, LPCWSTR shaderfileName) const
+{
+	char* compileErrors;
+	unsigned long bufferSize, i;
+	ofstream fout;
+
+	compileErrors = (char*)(errMsg->GetBufferPointer());
+
+	bufferSize = errMsg->GetBufferSize();
+
+	fout.open("shader-error.txt");
+
+	for (i = 0; i < bufferSize; i++)
+	{
+		fout << compileErrors[i];
+	}
+	fout.close();
+
+	MessageBoxW(hWnd, L"Error compiling shader.  Check shader-error.txt for message.", shaderfileName, MB_OK);
+}
